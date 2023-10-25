7469169bd25fb927fc9e7fa608aa80ccfdb5abff
Udit Tyagi <104336669+udittyagi03@users.noreply.github.com>
2023-10-25 20:59:13 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+    if (isset($_POST)){
+        $name = $_POST['name'];
+        $birthday = $_POST['birthday'];
+        $contact = $_POST['contact'];
+        $alternate_contact = $_POST['alternate_contact'];
+        $email = $_POST['email'];
+        $select_state = $_POST['select_state'];
+        $purpose = $_POST['purpose'];
+        $review = $_POST['review'];
+    }
+
+    ['']
+?>
